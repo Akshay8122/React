2c130885f8332734eb6e3e012f3cef11b0435d4f
Akshay <akshay.p@simformsolutions.com>
2022-02-16 11:22:54 +0530

    Creating a Toolchain from Scratch

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './main.tsx',
+  output: {
+    path: path.resolve(__dirname, '/bundle'),
+    filename: 'index_bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  devServer: {
+    // port: 6000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: [/node_modules|bower_components/, require.resolve('./index.html')],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+};
